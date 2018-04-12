49cf700649183c5a3c038866455f9365c0e9a5ec
Jesse Cogollo <jesse@adviceinteractive.com>
2018-04-11 21:27:52 -0500

    set data, done.

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## data:
 
-We are getting data from (public API).
+We are getting data from the meetup.com API using the endpoint: http://api.meetup.com/medellinjs/events?status=past.
+This end point retrieved the past meetups made for medellinjs community.
 
 ## methods
 
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const {
+  getData
+} = require('./lib/connection');
+
+const fnThen = (result) => {
+  console.log('result', result);
+  return result;
+};
+
+const fnCatch = (error) => {
+  console.log('error', error);
+  return error;
+};
+
+getData()
+  .then(fnThen)
+  .catch(fnCatch);
